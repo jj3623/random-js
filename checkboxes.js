6a8428d112d12ca0bb1cddb5b9c4fa9b956b4e8c
deez <102386676+jj3623@users.noreply.github.com>
2022-03-30 11:00:14 -0500

    Update checkboxes.js

--- checkboxes.js
+++ checkboxes.js
@@ -1,32 +1,14 @@
 let c = {};
+c.initialized = false;
 c.width = 0;
 c.height = 0;
 c.checks = [];
-c.initialized = false;
 
 c.init = function init(width = 20, height = 20) {
-  if(width < 1 || height < 1) throw RangeError();
-  if(document.getElementById('checks')) document.getElementById('checks').remove();
+  if(c.initialized) throw Error('Already initialized');
+  if(width < 1 || height < 1) throw RangeError('Invalid dimensions');
   c.width = width;
   c.height = height;
-  let checks = document.createElement('div');
-  checks.id = 'checks';
+  c.checks = [];
   
-  for(let x=0; x<width; x++) {
-    let c.checks[x] = [];
-    for(let y=0; y<height; y++) {
-      let check = document.createElement('input');
-      check.type = 'checkbox';
-      c.checks[x][y] = check;
-      checks.append(check);
-    }
-    let br = document.createElement('br');
-    checks.append(br);
-  }
-}
-
-c.reset = function reset() {
-  if(document.getElementById('checks')) {
-    for(let x=0; x<c.width; x++) {
-      for(let y=0; y<c.height; y++) {
-        
+  
