e980eb728394f7bc2eb593e4046761766a40af55
deez <102386676+jj3623@users.noreply.github.com>
2022-04-01 14:55:21 -0500

    Update nodeMgr.js

--- nodes/nodeMgr.js
+++ nodes/nodeMgr.js
@@ -8,8 +8,16 @@ nodes.maps.names = {};
 nodes.validateIP = function validateIP(ip) {
   if(typeof ip !== 'string') throw TypeError('Arguements invalid');
   
-  let regex = /([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/m;
-  return ip.match(regex);
+  let regex = /([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;
+  return regex.test(ip);
+}
+
+nodes.randomIP = function randomIP() {
+  let ip = '';
+  for(let i=0; i<4; i++) {
+    ip += Math.floor(Math.random()*255)+1 + '.';
+  }
+  return ip.slice(0, -1);
 }
 
 nodes.getNextId = function getNextId() {
@@ -22,4 +30,11 @@ nodes.getCount = function getCount() {
 }
 
 nodes.exists = function exists(id) {
-  if(typeof id !== 'number') throw Tye
+  if(typeof id !== 'number') throw TypeError('Arguements invalid');
+  
+  return typeof nodes.nodes[id] !== 'undefined';
+}
+
+nodes.create = function create(ip, name, password) {
+  if(typeof ip !== 'string' || typeof name !== 'string' || typeof password !== 'string') throw TypeError('Arguements invalid');
+  if(!nodes.validateIP(ip)) throw TypeError('Arguements invalid');
