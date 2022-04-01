de7a8ccdb4ec6604f27d3836a06bdb82d0937724
deez <102386676+jj3623@users.noreply.github.com>
2022-04-01 09:07:43 -0500

    Create script.js

---
+++ thing/script.js
@@ -0,0 +1,14 @@
+function addItem() {
+  let div = document.createElement('div');
+  let input1 = document.createElement('input');
+  let p = document.createElement('p');
+  let input2 = document.createElement('input');
+  let button1 = document.createElement('button');
+  let button2 = document.createElement('button');
+  let button3 = document.createElement('button');
+  
+  div.className = 'item';
+  input1.type = 'checkbox';
+  input2.placeholder = 'New name:';
+  input2.style.display = 'none';
+  button1.
