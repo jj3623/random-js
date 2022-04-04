b2cb2cbd169ea3a6f4538e1335b1b29a5643191f
deez <102386676+jj3623@users.noreply.github.com>
2022-04-04 13:38:35 -0500

    Update nodes-v2.js

--- nodes/nodes-v2.js
+++ nodes/nodes-v2.js
@@ -1,6 +1,13 @@
 let net = {
   networks: {
-    networks: {},
+    networks: {
+      0: {
+        id: 0,
+        parent: 0,
+        ip: '0.0.0.0',
+        nodes
+      }
+    },
     map: {},
     nextId: 0
   },
@@ -14,4 +21,6 @@ let net = {
 net.checkIp = function checkIp(ip) { //only checks syntax
   if(typeof ip !== 'string' || !ip) throw TypeError('Arguements invalid');
   
-  let regex
+  let regex = /^([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;
+  return regex.test(ip);
+}
