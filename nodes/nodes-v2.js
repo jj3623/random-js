831c8beebf13f12c536690b942d180b83c759acd
deez <102386676+jj3623@users.noreply.github.com>
2022-04-04 11:31:11 -0500

    Update nodes-v2.js

--- nodes/nodes-v2.js
+++ nodes/nodes-v2.js
@@ -1,1 +1,17 @@
+let net = {
+  networks: {
+    networks: {},
+    map: {},
+    nextId: 0
+  },
+  nodes: {
+    nodes: {},
+    map: {},
+    nextId: 0
+  }
+};
 
+net.checkIp = function checkIp(ip) { //only checks syntax
+  if(typeof ip !== 'string' || !ip) throw TypeError('Arguements invalid');
+  
+  let regex
