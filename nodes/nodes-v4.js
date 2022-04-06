d8eacfa3f965afb7b544a09768ed3bd49b96c32c
deez <102386676+jj3623@users.noreply.github.com>
2022-04-06 14:55:07 -0500

    Update nodes-v4.js

--- nodes/nodes-v4.js
+++ nodes/nodes-v4.js
@@ -1,1 +1,29 @@
+let net = {};
+net.nodes = {};
+net.nextId = 0;
 
+net.exists = function exists(id) {
+  if(typeof id !== 'number') throw TypeError('Arguements invalid');
+  
+  return typeof net.nodes[id] !== 'undefined';
+}
+
+net.create = function create(name) {
+  if(typeof name !== 'string' || !name) throw TypeError('Arguements invalid');
+  
+  let node = {};
+  node.id = net.nextId;
+  node.name = name;
+  node.ints = {};
+  
+  net.nodes[node.id] = node;
+  net.nextId++;
+  return node.id;
+}
+
+net.remove = function remove(id) {
+  if(typeof id !== 'number') throw TypeError('Arguements invalid');
+  if(!net.exists(id)) throw ReferenceError('Unknown node');
+  
+  let node = net.nodes[id];
+  for(let i=0; i<)
