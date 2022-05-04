2a1b0010ec40f9ccc300a74b6cb371ad3e7da237
deez <102386676+jj3623@users.noreply.github.com>
2022-05-04 09:00:32 -0500

    aeriufuhkqefwhiufwhrfk2hrfkw4hfkwlhrjflw4

--- net.js
+++ net.js
@@ -6,11 +6,11 @@ for(let i=0; i<25; i++) {
   net.grid[i] = new Array(25);
 }
 
-net.getNextId = function getNextId() {
+net.nodeGetNextId = function getNextId() {
   return net.nextId;
 }
 
-net.list = function list() {
+net.nodeList = function list() {
   let out = [];
   for(let i in net.nodes) {
     out[i] = net.nodes[i].name;
@@ -18,8 +18,31 @@ net.list = function list() {
   return out;
 }
 
-net.exists = function exists(id) {
+net.nodeExists = function exists(id) {
   if(typeof id !== 'number') throw TypeError('Arguements invalid');
   
   return typeof net.nodes[id] !== 'undefined';
+}
+
+net.createNode = function createNode(type, name, x, y) {
+  if(typeof type !== 'string' ||
+     typeof name !== 'string' ||
+     typeof x !== 'number' ||
+     typeof y !== 'number' ||
+     (type !== 'computer' && type !== 'router' && type !== 'switch') ||
+     !name ||
+     x < 0 ||
+     x > 24 ||
+     y < 0 ||
+     y > 24) {
+    throw TypeError('Arguements invalid');
+  }
+  if()
+}
+
+net.getType = function getType(id) {
+  if(typeof id !== 'number') throw TypeError('Arguements invalid');
+  if(!net.exists(id)) throw ReferenceError('Unknown node');
+  
+  return net.nodes[id].type;
 }
